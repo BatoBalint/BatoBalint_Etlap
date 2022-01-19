c0320272c02a4f4a3cd87552e08c3777cb229c4f
BatoBalint <balint.bato@gmail.com>
2022-01-19 15:35:17 +0100

    etel felvetel ablak hozzaadva/tesztelve

---
+++ src/main/java/com/example/etlap/AddFoodController.java
@@ -0,0 +1,20 @@
+package com.example.etlap;
+
+import javafx.scene.control.*;
+import javafx.fxml.FXML;
+
+public class AddFoodController {
+    @FXML
+    private Spinner priceInput;
+    @FXML
+    private TextField categoryInput;
+    @FXML
+    private TextField nameInput;
+    @FXML
+    private Button addBtn;
+
+    @FXML
+    public void addBtnClick() {
+
+    }
+}
--- src/main/java/com/example/etlap/WelcomeController.java
+++ src/main/java/com/example/etlap/WelcomeController.java
@@ -1,14 +1,41 @@
 package com.example.etlap;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class WelcomeController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableView menuTable;
+    @FXML
+    private Label descriptionTxt;
+    @FXML
+    private Button newFoodBtn;
+    @FXML
+    private Button deleteBtn;
+
+    @FXML
+    public void newFoodBtnClick() {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(EtlapApp.class.getResource("add-food-view.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Étel felvétel");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void deleteBtnClick() {
+
     }
 }
