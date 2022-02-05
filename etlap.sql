12d077cdd435b7c8eadd5075e9d6c0ae3cbc028c
BatoBalint <61137223+BatoBalint@users.noreply.github.com>
2022-02-05 21:20:41 +0100

    adatbazis commitolva

---
+++ etlap.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 05, 2022 at 09:20 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `etlap`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `etlap`
+--
+
+CREATE TABLE `etlap` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `leiras` text COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `ar` int(11) NOT NULL,
+  `kategoria_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- Dumping data for table `etlap`
+--
+
+INSERT INTO `etlap` (`id`, `nev`, `leiras`, `ar`, `kategoria_id`) VALUES
+(1, 'Húsleves', 'Tyúkhússal, Cérnametélttel', 1490, 1),
+(2, 'Bableves', 'Füstölt csülökkel', 1590, 1),
+(3, 'Gyros tál', 'Gyroshússal, hasábburgonyával, 5 féle salátával ', 1890, 2),
+(4, 'Velővel töltött borda', 'Kirántva, hasábburgonyával', 1890, 2),
+(5, 'Rib-Eye Steak', 'BBQ zöldbabbal', 4950, 2),
+(6, 'Tiramisu', '', 890, 3),
+(7, 'Palacsinta', 'Kakaós, Nutellás, Lekváros', 490, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategoria`
+--
+
+CREATE TABLE `kategoria` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategoria`
+--
+
+INSERT INTO `kategoria` (`id`, `nev`) VALUES
+(1, 'előétel'),
+(2, 'főétel'),
+(3, 'desszert');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `etlap`
+--
+ALTER TABLE `etlap`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kategoria_id` (`kategoria_id`);
+
+--
+-- Indexes for table `kategoria`
+--
+ALTER TABLE `kategoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `etlap`
+--
+ALTER TABLE `etlap`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `kategoria`
+--
+ALTER TABLE `kategoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `etlap`
+--
+ALTER TABLE `etlap`
+  ADD CONSTRAINT `kategoria_id` FOREIGN KEY (`kategoria_id`) REFERENCES `kategoria` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
